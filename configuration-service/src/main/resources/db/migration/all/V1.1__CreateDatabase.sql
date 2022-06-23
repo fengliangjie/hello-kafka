4a98eab521b2cc10d5ebe5f6cc0609d75c7e23af
liangjie.feng <13540603778@163.com>
2022-06-23 14:16:25 +0800

    update schema

--- configuration-service/src/main/resources/db/migration/all/V1.1__CreateDatabase.sql
+++ configuration-service/src/main/resources/db/migration/all/V1.1__CreateDatabase.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS public.iconnector_info
+CREATE TABLE IF NOT EXISTS config.iconnector_info
 (
     id bigserial NOT NULL,
     connector_id character varying(255) COLLATE pg_catalog."default" NOT NULL UNIQUE,
@@ -8,31 +8,27 @@ CREATE TABLE IF NOT EXISTS public.iconnector_info
     date_time bigint,
     enable boolean,
     CONSTRAINT iconnector_info_pkey PRIMARY KEY (id)
-)
+);
 
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.iconnector_info
+ALTER TABLE IF EXISTS config.iconnector_info
     OWNER to sigreen;
 
-CREATE TABLE IF NOT EXISTS public.iconnector_config
+CREATE TABLE IF NOT EXISTS config.iconnector_config
 (
     id bigserial NOT NULL,
     name character varying(255) COLLATE pg_catalog."default",
     info_id bigint,
     CONSTRAINT iconnector_config_pkey PRIMARY KEY (id),
     CONSTRAINT iconnector_info_config FOREIGN KEY (info_id)
-        REFERENCES public.iconnector_info (id) MATCH SIMPLE
+        REFERENCES config.iconnector_info (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
-)
+);
 
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.iconnector_config
+ALTER TABLE IF EXISTS config.iconnector_config
     OWNER to sigreen;
 
-CREATE TABLE IF NOT EXISTS public.iconnector_module
+CREATE TABLE IF NOT EXISTS config.iconnector_module
 (
     id bigserial NOT NULL,
     name character varying(255) COLLATE pg_catalog."default",
@@ -41,12 +37,10 @@ CREATE TABLE IF NOT EXISTS public.iconnector_module
     info_id bigint,
     CONSTRAINT iconnector_module_pkey PRIMARY KEY (id),
     CONSTRAINT iconnector_info_module FOREIGN KEY (info_id)
-        REFERENCES public.iconnector_info (id) MATCH SIMPLE
+        REFERENCES config.iconnector_info (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
-)
+);
 
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.iconnector_module
+ALTER TABLE IF EXISTS config.iconnector_module
     OWNER to sigreen;
