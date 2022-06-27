6baa0c7eae1f00970dcdabf0ce4f86ce96cfeaa0
liangjie.feng <13540603778@163.com>
2022-06-27 10:58:31 +0800

    update

--- message-handler/kafka-ssl/general-client-ssl.sh
+++ message-handler/kafka-ssl/general-client-ssl.sh
@@ -11,9 +11,9 @@ password=123456
 rm -rf $name
 mkdir $name
 
-keytool -genkey -keystore $name/$name.client.keystore.jks -validity 365 -storepass $password -keypass $password -keyalg RSA -dname "CN=$name" -storetype pkcs12
+keytool -genkey -keystore $name/$name.client.keystore.jks -validity 3650 -storepass $password -keypass $password -keyalg RSA -dname "CN=$name" -storetype pkcs12
 keytool -keystore $name/$name.client.keystore.jks -certreq -file $name/client-cert-sign-request -storepass $password -keypass $password
-openssl x509 -req -CA ca-cert -CAkey ca-key -in $name/client-cert-sign-request -out $name/client-signed-cert -days 365 -CAcreateserial -passin pass:$password
+openssl x509 -req -CA ca-cert -CAkey ca-key -in $name/client-cert-sign-request -out $name/client-signed-cert -days 3650 -CAcreateserial -passin pass:$password
 
 keytool -keystore $name/$name.client.truststore.jks -alias CARoot -import -file ca-cert -storepass $password -keypass $password -noprompt
 keytool -keystore $name/$name.client.keystore.jks -alias CARoot -import -file ca-cert -storepass $password -keypass $password -noprompt
--- message-handler/kafka-ssl/general-server-ssl.sh
+++ message-handler/kafka-ssl/general-server-ssl.sh
@@ -10,13 +10,13 @@ cd general-ssl-jks
 name=localhost
 password=123456
 #服务端生成密钥库和证书
-keytool -genkey -keystore kafka.server.keystore.jks -validity 365 -storepass $password -keypass $password -keyalg RSA -dname "CN=$name" -storetype pkcs12
+keytool -genkey -keystore kafka.server.keystore.jks -validity 3650 -storepass $password -keypass $password -keyalg RSA -dname "CN=$name" -storetype pkcs12
 keytool -keystore kafka.server.keystore.jks -certreq -file cert-file -storepass $password -keypass $password
 
 #配置CA，生成密钥和证书
-openssl req -new -newkey rsa:4096 -days 365 -x509 -subj "/CN=$name" -keyout ca-key -out ca-cert -nodes
+openssl req -new -newkey rsa:4096 -days 3650 -x509 -subj "/CN=$name" -keyout ca-key -out ca-cert -nodes
 #用CA的密钥和证书对服务端证书签名
-openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file -out cert-signed -days 365 -CAcreateserial -passin pass:$password
+openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file -out cert-signed -days 3650 -CAcreateserial -passin pass:$password
 
 #导入CA证书和签过名的服务端证书，并将CA证书加入truststore中
 keytool -keystore kafka.server.keystore.jks -alias CARoot -import -file ca-cert -storepass $password -keypass $password -noprompt
